6d090d09ddf12d8bcce98d5fcbf4f93f610f5f50
BrigitteFlores16 <macalupufloresyiyibrigitte@gmail.com>
2024-11-28 20:06:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+//import { useState } from 'react'
+import Header from './components/Header.jsx';
+import Blog from './components/Blog.jsx';
+import  Footer from'./components/Footer.jsx';
+
+function App() {
+   return (
+    <>
+   <Header/>
+   <Blog/>
+   <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Blog.jsx
@@ -0,0 +1,17 @@
+export default function Blog() {
+    return (
+      
+        <main className="card ">
+         <img src="./src/assets/img/600x400.png" className="card-img-top" alt="Post" />
+          <div className="card-body">
+            <h2 className="card-title">Titolo del Post</h2>
+            <p className="card-text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta minima consequatur odit voluptate cupiditate nesciunt. Molestiae, sequi ipsa? Sint enim officiis delectus porro corporis, eveniet nesciunt sunt tempora impedit exercitationem!
+            </p>
+            <button className="btn btn-warning">LEGGI DI PIÙ</button>
+          </div>
+        </main>
+     
+    );
+  }
+  
